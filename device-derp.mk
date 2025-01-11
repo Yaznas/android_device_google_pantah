DerpFest-Devices/device_google_pantah

pantah: Derp it
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

device-derp.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-derp
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \

Yaznas/android_device_google_pantah

device-derp.mk
@@ -4,7 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-derp
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \