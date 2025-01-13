73e0a5ceeea287124076ca9137e1103bf8d0487d
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 16:14:21 +0200

    Add missing IWLAN packages
    Change-Id: I671e4f057eb3fbe94f63dc16bf697867278e3eb2

--- device-derp.mk
+++ device-derp.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# IWLAN
+PRODUCT_PACKAGES += \
+    Iwlan
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
